49ff03732fe5019aaf7d0a66f23b43000f34d408
sachin-c0des <101795721+sachin-c0des@users.noreply.github.com>
2022-07-27 16:53:51 +0530

    Update options.spec.ts

--- test/options.spec.ts
+++ test/options.spec.ts
@@ -4,7 +4,7 @@ import * as path from 'path'
 
 const ext = process.platform === 'win32' ? '.exe' : ''
 
-// comment added here
+// comment added 
 describe('options', () => {
   describe('cwd', () => {
       it('should use process.cwd() if nothing is provided', () => {
